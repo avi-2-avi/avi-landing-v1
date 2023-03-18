cf872dfa793b891fb1e9b66511b5856ac63a993d
avi-2-avi <avicon95@protonmail.com>
2023-03-18 12:53:04 -0500

    Navbar toggle effect and absolute position

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 import { useRouter } from "next/router";
 import ToggleThemeButton from "./ToggleThemeButton";
 import MenuOutlinedIcon from "@mui/icons-material/MenuOutlined";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 import "animate.css";
 
@@ -12,10 +12,30 @@ const Navbar = () => {
   const { theme } = useTheme();
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   const router = useRouter();
+  const menuRef = useRef<HTMLDivElement>(null);
+  const toggleRef = useRef<HTMLDivElement>(null);
 
   const onToggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
   };
+
+  useEffect(() => {
+    const handleOutsideClick = (event: MouseEvent) => {
+      if (
+        menuRef.current &&
+        !menuRef.current.contains(event.target as Node) &&
+        toggleRef.current &&
+        !toggleRef.current.contains(event.target as Node)
+      ) {
+        setIsMenuOpen(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleOutsideClick);
+    return () => {
+      document.removeEventListener("mousedown", handleOutsideClick);
+    };
+  }, [menuRef, onToggleMenu]);
 
   return (
     <nav
@@ -74,36 +94,39 @@ const Navbar = () => {
       </div>
       <div className="flex space-x-4">
         <ToggleThemeButton />
-        <MenuOutlinedIcon
-          className="w-7 h-7 cursor-pointer md:hidden block"
-          onClick={onToggleMenu}
-        />
+        <div ref={toggleRef}>
+          <MenuOutlinedIcon
+            className="w-7 h-7 cursor-pointer md:hidden block"
+            onClick={onToggleMenu}
+          />
+        </div>
         <div
+          ref={menuRef}
           className={`${isMenuOpen ? "block" : "hidden"} 
           text-background-light dark:text-background-dark
-          z-10 mt-10 fixed flex flex-col md:hidden 
+          z-10 mt-10 absolute flex flex-col md:hidden 
           animate__animated animate__fadeIn animate__faster`}
         >
           <Link
-            className="bg-yellow-light dark:bg-yellow-dark px-3 py-2"
+            className="bg-yellow-light dark:bg-yellow-dark hover:opacity-95 px-3 py-2"
             href="/"
           >
             About
           </Link>
           <Link
-            className="bg-red-light dark:bg-red-dark px-3 py-2"
+            className="bg-red-light dark:bg-red-dark hover:opacity-95 px-3 py-2"
             href="/works"
           >
             Works
           </Link>
           <Link
-            className="bg-blue-light dark:bg-blue-dark px-3 py-2"
+            className="bg-blue-light dark:bg-blue-dark hover:opacity-95 px-3 py-2"
             href="/tools"
           >
             Tools
           </Link>
           <Link
-            className="bg-foreground-light dark:bg-foreground-dark px-3 py-2"
+            className="bg-foreground-light dark:bg-foreground-dark hover:opacity-95 px-3 py-2"
             href="https://github.com/avi-2-avi"
           >
             Source
