6e89481a0986f33c1d2660ec21388849ea81930c
avi-2-avi <avicon95@protonmail.com>
2023-03-31 18:12:50 -0500

    Initial works list - March 2023

--- src/data/worksList.ts
+++ src/data/worksList.ts
@@ -13,51 +13,86 @@ interface Work {
 
 const worksList: Work[] = [
   {
-    year: 2022,
-    title: "Notes in Django",
+    year: 2021,
+    title: "Chess++",
     made_at: "-",
-    built_with: ["React", "Django"],
+    built_with: ["C++"],
     links: {
-      source: "https://github.com/avi-2-avi",
-      presentation: "https://google.com",
+      source: "https://github.com/avi-2-avi/TrabajoFinalDamas",
+    },
+  },
+  {
+    year: 2021,
+    title: "Karnaugh Map Solver",
+    made_at: "-",
+    built_with: ["Python", "Pandas", "Numpy", "Tkinter"],
+    links: {
+      source: "https://github.com/avi-2-avi/karnaugh-gui",
+    },
+  },
+  {
+    year: 2021,
+    title: "SiFind",
+    made_at: "-",
+    built_with: ["Python", "Pandas", "Numpy", "Tkinter"],
+    links: {
+      source: "https://github.com/avi-2-avi/sifind",
     },
   },
   {
     year: 2022,
-    title: "Notes in Django",
+    title: "Landing Page - Version 0",
     made_at: "-",
-    built_with: ["React", "Django"],
+    built_with: ["Svelte", "Typescript", "HTML", "CSS"],
     links: {
-      source: "https://github.com/avi-2-avi",
-      presentation: "https://google.com",
-    },
-  },
-  {
-    year: 2020,
-    title: "Notes in Django 2",
-    made_at: "-",
-    built_with: ["React", "Django"],
-    links: {
-      source: "https://github.com/avi-2-avi",
+      source: "https://github.com/avi-2-avi/avi-landing-v0",
+      presentation: "https://avi-2-avi.github.io/avi-landing-v0",
     },
   },
   {
     year: 2022,
-    title: "Notes in Django",
+    title: "Fab Academy Project Blog",
     made_at: "-",
-    built_with: ["React", "Django"],
+    built_with: ["Bootstrap", "HTML", "CSS"],
     links: {
-      presentation: "https://google.com",
+      source: "https://github.com/avi-2-avi/fab-academy-blog",
+      presentation: "https://avi-2-avi.github.io/fab-academy-blog/",
     },
   },
   {
     year: 2022,
-    title: "Notes in Django",
+    title: "Inventory Module",
     made_at: "-",
-    built_with: ["React", "Django"],
+    built_with: [".NET", "C#", "React.js", "Javascript"],
     links: {
-      source: "https://github.com/avi-2-avi",
-      presentation: "https://google.com",
+      source: "https://github.com/avi-2-avi/inventory-module",
+    },
+  },
+  {
+    year: 2023,
+    title: "Django React Notes",
+    made_at: "-",
+    built_with: ["Django", "Python", "React.js", "Typescript"],
+    links: {
+      source: "https://github.com/avi-2-avi/django-react-notes",
+    },
+  },
+  {
+    year: 2023,
+    title: "Landing Page - Version 1",
+    made_at: "-",
+    built_with: ["Next.js", "Typescript"],
+    links: {
+      source: "https://github.com/avi-2-avi/avi-landing-v1",
+    },
+  },
+  {
+    year: 2023,
+    title: "Affinity Roofing LLC - Landing Page",
+    made_at: "Affinity Roofing LLC",
+    built_with: ["Astro", "Javascript", "AWS"],
+    links: {
+      presentation: "https://www.affinityroofingllc.com/",
     },
   },
 ];
