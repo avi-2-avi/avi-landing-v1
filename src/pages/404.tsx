1ffe47ae180a1fb193b2a75d6013089d4d3523a1
avi-2-avi <avicon95@protonmail.com>
2023-03-31 18:52:01 -0500

    Appostrophe correction

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -24,7 +24,7 @@ const NotFound = () => {
         </div>
         <div className="flex flex-col text-center">
           <h1 className="text-5xl mt-8 mb-6">404</h1>
-          <p>The page you're looking for does not exist.</p>
+          <p>The page you are looking for does not exist.</p>
           <div className="bg-foreground-light dark:bg-foreground-dark h-0.5 opacity-10 w-60 xs:w-96 mx-auto my-8" />
           <button className="mx-auto py-2 w-40 rounded-md hover:opacity-90 font-medium bg-blue-light dark:bg-blue-dark text-background-light dark:text-background-dark mb-12">
             <Link href="/">Return to home</Link>
